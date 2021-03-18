c0ebaa9f955ad603861b35e72d9b69ceb1f6b8a5
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2021-03-17 22:11:15 -0300

    Initial commit

---
+++ 02-react-concepts/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import api from './services/api'
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const newRepository = {
+      title: "REPOSITORIO TESTE",
+      url: "https://github.com/gabrielkuhn/DesafioGoStack-ConceitosNode",
+      techs: ["NodeJS", "React"]
+    }
+    const response = await api.post('repositories', newRepository)
+    const repository = response.data
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`)
+    setRepositories(repositories.filter(repository => repository.id !== id))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {
+          repositories.map((repository) => {
+            return (
+              <li key={repository.id}>
+                {repository.title}
+
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          })
+        }   
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-node-fundamentals/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,61 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
